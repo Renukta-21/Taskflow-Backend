7792339951eab5ffc37e4fed7f579660cb136ac9
Daniel Urbina <edu211004@gmail.com>
2024-11-25 15:17:32 -0600

    populating user

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/tasks')
 
 tasksRouter.get('/', async(req,res)=>{
     const user = req.user
-    const tasks = await Task.find({ user: user._id} )
+    const tasks = await Task.find({ user: user._id}).populate('user', {username:1, email:1})
     if(!tasks){
         return res.status(404).send({error: 'No user tasks found'})
     }
